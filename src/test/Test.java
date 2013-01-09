fac06c48dfd48ce74cd254a49819dd189fcf24d3
arjan doets <arjan-doets@live.nl>
2013-01-09 17:27:30 +0100

    test2

--- src/test/Test.java
+++ src/test/Test.java
@@ -16,6 +16,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("dit is een test");
         
-        //dit werkt volgens mij nu wel
+        //dit werkt volgens mij nu wel test
     }
 }
