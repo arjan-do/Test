edbb3db23cd255522137bca594791fcff5c2d396
arjan-do <arjan-doets@live.nl>
2013-01-09 17:22:13 +0100

    First edit

--- src/test/Test.java
+++ src/test/Test.java
@@ -14,6 +14,6 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("dit is een test");
     }
 }
